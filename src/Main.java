d20482a9d45835cbfd1ed6925174f6d1a2096561
Georgy <georgy.matskevich@gmail.com>
2022-03-22 17:35:54 +0300

    Payment page

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (String[]args){
-
+        payment payment = new payment();
+        System.out.println(payment.getPay());
     }
 }
---
+++ src/payment.java
@@ -0,0 +1,8 @@
+public class payment {
+   int pay = 3;
+
+   public int getPay(){
+      return pay;
+   }
+
+}
